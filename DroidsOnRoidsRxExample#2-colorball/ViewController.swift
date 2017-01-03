a0ea9b59a41a1d086c5fb143b73987069fd343ad
Patrick <patrickbellot77@gmail.com>
2017-01-03 10:01:21 -0600

    Initial Commit

---
+++ DroidsOnRoidsRxExample#2-colorball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DroidsOnRoidsRxExample#2-colorball
+//
+//  Created by Patrick Bellot on 1/3/17.
+//  Copyright © 2017 Bell OS, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
